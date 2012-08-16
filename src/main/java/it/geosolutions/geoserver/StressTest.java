8251e75a6e2122b1e103e1db35ba194af5b62a7f
carlo cancellieri <cancellieri@CIO174.hq.un.fao.org>
2012-08-16 11:16:05 +0200

    improving code

--- src/main/java/it/geosolutions/geoserver/StressTest.java
+++ src/main/java/it/geosolutions/geoserver/StressTest.java
@@ -28,9 +28,11 @@ public class StressTest {
 
 	/**
 	 * USAGE:<br>
-	 * java StressTest [getCapabilities.xml] [getCapabilities.csv] [params.properties] [] [jmeeter.ftl TEMPLATE_DIR]
-	 * EXAMPLE:
-	 * java StressTest src/main/resources/getCapabilities.xml src/main/resources/getCapabilities.csv src/main/resources/params.properties jmeeter.ftl src/main/resources/  
+	 * java StressTest [getCapabilities.xml] [getCapabilities.csv]
+	 * [params.properties] [] [jmeeter.ftl TEMPLATE_DIR] EXAMPLE: java
+	 * StressTest src/main/resources/getCapabilities.xml
+	 * src/main/resources/getCapabilities.csv
+	 * src/main/resources/params.properties jmeeter.ftl src/main/resources/
 	 * 
 	 * @param args
 	 * @throws IOException
@@ -42,44 +44,48 @@ public class StressTest {
 			TemplateException {
 
 		File sourceDom;
-		if (args.length>0 && args[0]!=null)
+		if (args.length > 0 && args[0] != null)
 			sourceDom = new File(args[0]);
 		else
 			sourceDom = new File("src/main/resources/getCapabilities.xml");
-		
+
 		File destCSV;
-		if (args.length>1 && args[1]!=null)
+		if (args.length > 1 && args[1] != null)
 			destCSV = new File(args[1]);
 		else
 			destCSV = new File("src/main/resources/getCapabilities.csv");
-		
+
 		File properties;
-		if (args.length>2 && args[2]!=null)
+		if (args.length > 2 && args[2] != null)
 			properties = new File(args[2]);
 		else
 			properties = new File("src/main/resources/params.properties");
 		Properties prop = new Properties();
 		prop.load(new FileReader(properties));
-		
+
 		String templateFileName;
-		if (args.length>3 && args[3]!=null)
+		if (args.length > 3 && args[3] != null)
 			templateFileName = args[3];
 		else
 			templateFileName = "jmeeter.ftl";
-		
+
 		File baseDir;
-		if (args.length>4 && args[4]!=null)
+		if (args.length > 4 && args[4] != null)
 			baseDir = new File(args[4]);
 		else
 			baseDir = new File("src/main/resources/");
-		
+
 		/* ------------------------------------------------------------------- */
 		/* You should do this ONLY ONCE in the whole application life-cycle: */
 		final Template temp = setup(baseDir, templateFileName);
-		
+
 		/* Create a data-model */
 		final Map root = new HashMap();
-		Tools stressTest=new Tools();
+
+		/* Merge data-model with template */
+		Writer writer = null;
+		Writer bwriter = null;
+		Tools stressTest = new Tools();
 		try {
 			DocumentBuilderFactory factory = DocumentBuilderFactory
 					.newInstance();
@@ -90,6 +96,15 @@ public class StressTest {
 			root.put("doc", doc);
 			root.put("properties", prop);
 			root.put("StressTest", stressTest);
+
+			writer = new FileWriter(destCSV);
+			bwriter = new BufferedWriter(writer);
+			temp.process(root, bwriter);
+
+			System.out.println("WCS Count: " + stressTest.getWCSCount());
+			System.out.println("WFS Count: " + stressTest.getWFSCount());
+			System.out.println("LayerGroup Count: "
+					+ stressTest.getLayerGroupCount());
 			
 		} catch (SAXException e) {
 			// TODO Auto-generated catch block
@@ -100,25 +115,55 @@ public class StressTest {
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-
-		/* Merge data-model with template */
-		Writer writer=null;
-		Writer bwriter=null;
-		try {
-			writer=new FileWriter(destCSV);
-			bwriter=new BufferedWriter(writer);
-			temp.process(root, bwriter);
-			
-		} catch (IOException e){
+		} finally {
 			IOUtils.closeQuietly(bwriter);
 			IOUtils.closeQuietly(writer);
 		}
-				
-		System.out.println("WCS Count: "+stressTest.getWCSCount());
-		System.out.println("WFS Count: "+stressTest.getWFSCount());
-		System.out.println("LayerGroup Count: "+stressTest.getLayerGroupCount());
 
+
+	}
+
+	/**
+	 * tools used by the template to randomize BB and w,h
+	 * 
+	 * @author cancellieri
+	 * 
+	 */
+	public static class Tools {
+
+		private Random rand = new Random();
+
+		private long WFSCount = 0;
+		private long WCSCount = 0;
+		private long LayerGroupCount = 0;
+
+		public double rand(double min, double max) {
+			return (rand.nextDouble()) * (max - min) + min;
+		}
+
+		public long getWFSCount() {
+			return WFSCount;
+		}
+
+		public void setWFSCount(long wFSCount) {
+			WFSCount = wFSCount;
+		}
+
+		public long getWCSCount() {
+			return WCSCount;
+		}
+
+		public void setWCSCount(long wCSCount) {
+			WCSCount = wCSCount;
+		}
+
+		public long getLayerGroupCount() {
+			return LayerGroupCount;
+		}
+
+		public void setLayerGroupCount(long layerGroupCount) {
+			LayerGroupCount = layerGroupCount;
+		}
 	}
 
 	private static Template setup(final File baseDir,
@@ -136,44 +181,4 @@ public class StressTest {
 		Template temp = cfg.getTemplate(templateFileName);
 		return temp;
 	}
-
-	/**
-	 *  tools used by the template to randomize BB and w,h
-	 * @author cancellieri
-	 *
-	 */
-	public static class Tools {
-		
-
-		private Random rand=new Random();
-
-		private long WFSCount=0;
-		private long WCSCount=0;
-		private long LayerGroupCount=0;
-		
-		public double rand(double min, double max){
-			return (rand.nextDouble()) * (max-min) + min;
-		}
-		
-		public long getWFSCount() {
-			return WFSCount;
-		}
-		public void setWFSCount(long wFSCount) {
-			WFSCount = wFSCount;
-		}
-		public long getWCSCount() {
-			return WCSCount;
-		}
-		public void setWCSCount(long wCSCount) {
-			WCSCount = wCSCount;
-		}
-		public long getLayerGroupCount() {
-			return LayerGroupCount;
-		}
-		public void setLayerGroupCount(long layerGroupCount) {
-			LayerGroupCount = layerGroupCount;
-		}
-	}
-	
-	
 }
