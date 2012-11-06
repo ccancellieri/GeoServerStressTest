31315e33f22a434a14a672f3265118914ffe907a
ccancellieri <ccancellieri@hotmail.com>
2012-11-06 12:57:56 +0100

    added inline parameter capabilities

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 REM mvn clean install eclipse:clean eclipse:eclipse -DdownloadSources=true -DdownloadJavadocs=true -Declipse.addVersionToProjectName=true -o 
 
-mvn clean package integration-test pre-site -DargLine="-Djmeter.target.host=hqltstcdrgeo1.hq.un.fao.org"
+mvn clean package integration-test pre-site -Djmeter.target.host=hqltstcdrgeo1.hq.un.fao.org -Djmeter.target.port=8080 -Djmeter.thread.number=10 -Djmeter.loop.count=1
